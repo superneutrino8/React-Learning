6b34d0e58e7163a30a49485af629877f59d2048a
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-02-20 19:57:07 +0530

    Added Person Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hello React!</p>
+        <Person />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +23,7 @@ function App() {
         </a>
       </header>
     </div>
+    // React.createElement('div', {className: 'App'}, React.createElement('h1', null, `Hello React!!!!`))
   );
 }
 
