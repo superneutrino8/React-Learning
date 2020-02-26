5b9b45ba714f6fcca1640b51d7c6d3f14e29db44
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-02-26 20:47:20 +0530

    added dynamic list property

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ class App extends Component {
     super(props);
     this.state = {
       persons: [
-        { name: 'Max', age: 27 },
-        { name: 'Sam', age: 24 },
-        { name: 'Tim', age: 52 }
-      ]
+        { id: 'asd1', name: 'Max', age: 27 },
+        { id: 'asd2', name: 'Sam', age: 24 },
+        { id: 'asd3', name: 'Tim', age: 52 }
+      ],
+      toggleView: true
     }
   }
   switchNameHandler = (newName) => {
@@ -34,6 +35,19 @@ class App extends Component {
     })
   }
 
+  toggleViewFunc = () => {
+    const tempValue = this.state.toggleView;
+    this.setState({ toggleView: !tempValue })
+  }
+
+  deleteTextHandler = (index) => {
+    const temp = [...this.state.persons]; //making copy of original state because we need to preserve original state as a part of React efficiency
+    temp.splice(index, 1); 
+    this.setState({
+      persons: temp
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -44,17 +58,34 @@ class App extends Component {
         </p>
           <p>Hello React!</p>
           <button className='btn' onClick={this.switchNameHandler.bind(this, 'Maximilian!!')}>Switch Name</button> {/*Passed method reference using bind*/}
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={() => this.switchNameHandler('Max!!!')} 
-            changed={this.nameChangedHandler}/> {/*Passed method reference using arrow*/}
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} />
+          <br></br>
+          <button className='btn' onClick={this.toggleViewFunc}>Toggle Persons</button> {/*Conditional View*/}
+          {
+            this.state.toggleView ?
+              <div>
+                {/* <Person
+                  name={this.state.persons[0].name}
+                  age={this.state.persons[0].age} />
+                <Person
+                  name={this.state.persons[1].name}
+                  age={this.state.persons[1].age}
+                  click={() => this.switchNameHandler('Max!!!')}
+                  changed={this.nameChangedHandler} /> {/*Passed method reference using arrow*/}
+                {/* <Person
+                  name={this.state.persons[2].name}
+                  age={this.state.persons[2].age} /> */}
+                {this.state.persons.map((person, index) => {
+                  return <Person
+                    click={() => this.deleteTextHandler(index)}
+                    name={person.name}
+                    age={person.age}
+                    key={person.id}
+                  />
+                })}
+              </div>
+              : null
+          }
+          <br />
           <a
             className="App-link"
             href="https://reactjs.org"
